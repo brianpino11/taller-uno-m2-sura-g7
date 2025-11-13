ba9c3c470c12a6cb5c2ba709645f8eac3799a3af
brianpino11 <brianpino015@gmail.com>
2025-11-13 15:04:27 -0500

    Ejercicio 2: Pila de Tareas Pendientes (LIFO)

--- BrahianEstibenPinoPrado/script.js
+++ BrahianEstibenPinoPrado/script.js
@@ -1,6 +1,17 @@
-//Ejercicio #1
+//Ejercicio 1: Lista del Súpermercado
+
 let compras = ["Manzanas"];
 compras.push("Leche", "Pan");
 compras.unshift("Huevos");
 compras.splice(1, 1);
 console.log(compras);
+
+// Ejercicio 2: Pila de Tareas Pendientes (LIFO)
+
+let tareas = ["Revisar emails"];
+tareas.unshift("Llamar al cliente");
+tareas.unshift("Preparar reporte");
+tareas.shift();
+tareas.shift();
+console.log(tareas);
+
